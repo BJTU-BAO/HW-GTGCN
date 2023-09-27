2e02a0a6838799ccc9970f19f7af3365fdc3b96f
BJTU-BAO <143979897+BJTU-BAO@users.noreply.github.com>
2023-09-27 18:28:16 +0800

    Add files via upload

---
+++ tools/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Copyright 2020 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""
+Function:
+This file mainly set up for op_gen whl package.
+"""
+
+import os
+import stat
+from setuptools import setup
+os.environ['SOURCE_DATE_EPOCH'] = str(int(os.path.getctime(os.path.realpath(__file__))))
+
+__version__ = "0.1"
+
+
+with os.fdopen(os.open("MANIFEST.in", os.O_WRONLY | os.O_CREAT, stat.S_IWUSR | stat.S_IRUSR), "w") as fo:
+    fo.write("recursive-include ../op_gen/template *\n"
+             "recursive-include ../op_gen/json_template *\n"
+             "recursive-include ../op_gen/config *\n")
+
+setup_kwargs = {
+    "include_package_data": True
+}
+
+
+def read_txt(file_name):
+    """
+    read from file
+    """
+    with open(file_name) as file_object:
+        return file_object.read()
+
+
+setup(
+    name="op_gen",
+    version=__version__,
+    scripts=['./msopgen'],
+    zip_safe=False,
+    package_dir={'': '../'},
+    packages=['op_gen', 'op_gen/interface'],
+    install_requires=[],
+    license=read_txt("LICENSE"),
+    include_package_data=True
+)
